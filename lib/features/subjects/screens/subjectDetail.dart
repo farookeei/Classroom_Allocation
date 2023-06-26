f0ada441062f2526271d26a2aa8ec04b866fc2e1
farookeei <farookyj10@gmail.com>
2023-06-26 10:47:55 +0530

    fix:ui updated

--- lib/features/subjects/screens/subjectDetail.dart
+++ lib/features/subjects/screens/subjectDetail.dart
@@ -7,6 +7,9 @@ import 'package:test_sample/features/students/cubit/students_cubit.dart';
 import 'package:test_sample/features/subjects/cubit/subjects_cubit.dart';
 import 'package:test_sample/layout/layout_scaffold.dart';
 
+import '../../../themes/color_variables.dart';
+import '../../../widgets/appbar/custom_appbar.dart';
+import '../../../widgets/listtile_builder.dart.dart';
 import '../../../widgets/progress_indicator/custom_progress_indicator.dart';
 
 class SubjectDetail extends StatelessWidget {
@@ -18,35 +21,37 @@ class SubjectDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<SubjectsCubit>().getSubjectDetail(id: id);
     return LayoutScaffold(
+        backgroundColor: ReplyColors.neutralLight,
+        customAppBar: const CustomAppBar(title: "Subject Details"),
         child: SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-      child: BlocBuilder<SubjectsCubit, SubjectsState>(
-        builder: (context, state) {
-          if (state.isLoading) {
-            return const Center(child: CustomProgressIndicator());
-          } else if (state.isError) {
-            return Center(
-              child: Text(state.errorMessage.toString()),
-            );
-          }
-          return Column(
-            children: [
-              Text(
-                state.subjectDetail!.name!,
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              Text(
-                state.subjectDetail!.credits.toString(),
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              Text(
-                state.subjectDetail!.teacher!,
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-            ],
-          );
-        },
-      ),
-    ));
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+          child: BlocBuilder<SubjectsCubit, SubjectsState>(
+            builder: (context, state) {
+              if (state.isLoading) {
+                return const Center(child: CustomProgressIndicator());
+              } else if (state.isError) {
+                return Center(
+                  child: Text(state.errorMessage.toString()),
+                );
+              }
+              return Column(
+                children: [
+                  SettingsListTileBuilder(items: [
+                    SettingsTileModel(
+                        title: "Name", trailing: state.subjectDetail!.name!),
+                    SettingsTileModel(
+                      title: "Credits",
+                      trailing: state.subjectDetail!.credits.toString(),
+                    ),
+                    SettingsTileModel(
+                      title: "Teacher",
+                      trailing: state.subjectDetail!.teacher!,
+                    )
+                  ])
+                ],
+              );
+            },
+          ),
+        ));
   }
 }
